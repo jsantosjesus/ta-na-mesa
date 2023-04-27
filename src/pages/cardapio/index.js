45f32702686c1ea3de54d67cfbb5cdac153ca8bb
Jadson Santos <jadsoonbol@gmail.com>
2023-04-27 19:25:31 -0300

    com Erro

--- src/pages/cardapio/index.js
+++ src/pages/cardapio/index.js
@@ -5,11 +5,36 @@ import MenuBottom from '../../components/menuBottom';
 function Cardapio() {
     const estabelecimento = "Pizzaria Mão de Pilão";
 
+    const [produtos, setProdutos] = useState([{
+        id: 1,
+        imagem: "src",
+        nome: "Pizza de frango",
+        Descricao: "Tomate, cebola, Queijo e frango",
+        preco: 29.99
+    },
+{
+        id: 1,
+        imagem: "src",
+        nome: "Pizza de frango",
+        Descricao: "Tomate, cebola, Queijo e frango",
+        preco: 29.99
+    }])
+
+    const [searchTerm, setSearchTerm] = useState("");
+    const [filteredData, setFilteredData] = useState([]);
+
+    useEffect(() => {
+        filterData();
+    }, [searchTerm]);
+
     return (
         <div className='bodyCardapio'>
             <MenuBottom />
             <div className='headCardapio'>
                 <h1>{estabelecimento}</h1>
+            </div>
+            <div className='persquisaCardapio'>
+                <input type="text" placeholder="Pesquisar" value={searchTerm} onChange={e => setSearchTerm(e.target.value)}></input>
             </div>
         </div>
     );
