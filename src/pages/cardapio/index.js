0213311ae4ab141d20b6f41b8e95b1e4f82590b9
Jadson Santos <jadsoonbol@gmail.com>
2023-04-28 07:11:49 -0300

    pesquisa em pagina cardapio

--- src/pages/cardapio/index.js
+++ src/pages/cardapio/index.js
@@ -10,18 +10,46 @@ function Cardapio() {
         imagem: "src",
         nome: "Pizza de frango",
         Descricao: "Tomate, cebola, Queijo e frango",
-        preco: 29.99
+        preco: 59.99,
+        categoria: 'pizza'
     },
-{
-        id: 1,
+    {
+        id: 2,
         imagem: "src",
-        nome: "Pizza de frango",
-        Descricao: "Tomate, cebola, Queijo e frango",
-        preco: 29.99
+        nome: "Pizza de Lombo Tamanho Família",
+        Descricao: "Tomate, cebola, Queijo e lombo",
+        preco: 49.99,
+        categoria: 'pizza'
+    },
+    {
+        id: 3,
+        imagem: "src",
+        nome: "Cerveja Budwiser 200ml",
+        Descricao: "A melhor",
+        preco: 9.99,
+        categoria: 'bebidas'
+    },
+    {
+        id: 4,
+        imagem: "src",
+        nome: "Hamburguer Americano",
+        Descricao: "Carne Bovina, Tomate, Cebola, Alface, Queijo",
+        preco: 4.99,
+        categoria: 'hamburguer'
     }])
 
     const [searchTerm, setSearchTerm] = useState("");
     const [filteredData, setFilteredData] = useState([]);
+
+    const filterData = () => {
+        let results = produtos;
+
+        if (searchTerm !== "") {
+            results = produtos.filter(object => object.nome.toLowerCase().includes(searchTerm.toLowerCase()))
+        };
+
+        setFilteredData(results);
+    }
 
     useEffect(() => {
         filterData();
@@ -33,8 +61,28 @@ function Cardapio() {
             <div className='headCardapio'>
                 <h1>{estabelecimento}</h1>
             </div>
-            <div className='persquisaCardapio'>
-                <input type="text" placeholder="Pesquisar" value={searchTerm} onChange={e => setSearchTerm(e.target.value)}></input>
+            <div className='pesquisaCardapio'>
+                <input type="text" placeholder="Pesquise aqui" value={searchTerm} onChange={e => setSearchTerm(e.target.value)}></input>
+            </div>
+            <div className='produtosCardapio'>
+               {filteredData.map((object, id) => (
+                            <div >
+                                <div className="produtos" 
+                                // onClick={() => handleClick(object)}
+                                >
+
+                                    <p>{object.nome}</p>
+                                    <p>R$ {object.preco.toFixed(2).replace(".", ",")}</p>
+                                    <p>{object.categoria}</p>
+                                    <p>{object.imagem}</p>
+                                </div>
+                                {/* <Popup object={object} /> */}
+                            </div>
+
+                        )
+
+                        )
+                        } 
             </div>
         </div>
     );
